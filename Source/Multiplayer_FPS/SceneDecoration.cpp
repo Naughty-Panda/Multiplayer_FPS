1f23fea5d14215e94479dc74cbfbc87f012c76ab
Naughty Panda <hateme1990@gmail.com>
2022-04-25 23:17:53 +0300

    Added SceneDecoration and SceneDecorationManager.

---
+++ Source/Multiplayer_FPS/SceneDecoration.cpp
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SceneDecoration.h"
+#include "Net/UnrealNetwork.h"
+
+// Sets default values
+ASceneDecoration::ASceneDecoration()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root Component"));
+
+	DecorationMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Decoration Mesh"));
+	DecorationMesh->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ASceneDecoration::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ASceneDecoration::LoadResources()
+{
+	Multicast_LoadResources();
+}
+
+void ASceneDecoration::Client_LoadResources_Implementation()
+{
+	DecorationMesh->SetStaticMesh(DecorationMeshRef.IsPending() ? DecorationMeshRef.LoadSynchronous() : DecorationMeshRef.Get());
+}
+
+void ASceneDecoration::Multicast_LoadResources_Implementation()
+{
+	DecorationMesh->SetStaticMesh(DecorationMeshRef.IsPending() ? DecorationMeshRef.LoadSynchronous() : DecorationMeshRef.Get());
+}
+
+void ASceneDecoration::Server_LoadResources_Implementation()
+{
+	DecorationMesh->SetStaticMesh(DecorationMeshRef.IsPending() ? DecorationMeshRef.LoadSynchronous() : DecorationMeshRef.Get());
+}
---
+++ Source/Multiplayer_FPS/SceneDecoration.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "SceneDecoration.generated.h"
+
+UCLASS()
+class MULTIPLAYER_FPS_API ASceneDecoration : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Components")
+	TSoftObjectPtr<UStaticMesh> DecorationMeshRef;
+
+private:
+	UPROPERTY()
+	UStaticMeshComponent* DecorationMesh;
+
+public:
+	// Sets default values for this actor's properties
+	ASceneDecoration();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void LoadResources();
+
+private:
+	UFUNCTION(Client, Reliable)
+	void Client_LoadResources();
+
+	UFUNCTION(Server, Reliable)
+	void Server_LoadResources();
+
+	UFUNCTION(NetMulticast, Reliable)
+	void Multicast_LoadResources();
+};
